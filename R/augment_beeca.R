509d3f9d3851e9af78fe7783769a1de55e9139ba
bailliem <bailliem@gmail.com>
2024-07-26 22:51:11 +0200

    draft: sketch basic function

--- R/augment_beeca.R
+++ R/augment_beeca.R
@@ -1,3 +1,16 @@
+#' Augment original data with counter factual predictions
+#'
+#'
+#' @examples
+#' ## Set treatment to a factor
+#' trial01$trtp <- factor(trial01$trtp)
+#'
+#' ## Fit a logistic regression working model and pass it to beeca
+#' fit1 <- glm(aval ~ trtp + bl_cov, family="binomial", data=trial01) |>
+#'   get_marginal_effect(trt="trtp", method="Ye", contrast="diff")
+#'
+#'   augment_beeca(fit1)
+#'
 #' @export
 augment_beeca <- function(x) {
   data <- .get_data(x)
