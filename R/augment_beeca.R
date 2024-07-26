90a138d89c24285c3e81da11b33f02b891228060
bailliem <bailliem@gmail.com>
2024-07-26 22:49:48 +0200

    draft: sketch basic function

--- R/augment_beeca.R
+++ R/augment_beeca.R
@@ -3,6 +3,13 @@ augment_beeca <- function(x) {
   data <- .get_data(x)
 
   data <- dplyr::as_tibble(data)
+  predictions <- dplyr::as_tibble(x$counterfactual.predictions)
+  data <- dplyr::bind_cols(data, predictions)
+
+  original <- x$data
+
+  data <- original |> dplyr::left_join(data)
+
 
   return(data)
 }
